862615ea1b4586c673b61ee0180b38d89eaa8ea5
parkermmr <parkerrennie@outlook.com>
2024-08-14 17:51:04 +1000

    -> Added logic for LoginScreenController.

--- src/main/java/com/example/pixlpos/controllers/LoginScreenController.java
+++ src/main/java/com/example/pixlpos/controllers/LoginScreenController.java
@@ -1,4 +1,58 @@
 package com.example.pixlpos.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
 public class LoginScreenController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    private void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Login Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        // Ensure admin credentials work regardless of what is in DataStore
+        if ("admin".equals(username) && "admin".equals(password)) {
+            loadScene("/fxml/admin-console.fxml", "Admin Console");
+            return;
+        }
+        else {
+            showErrorDialog("Invalid username or password");
+        }
+    }
+
+    private void loadScene(String fxmlPath, String title) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
