f8b0a354e43f3e6b4c84b58170023068eb1b24cf
parkermmr <parkerrennie@outlook.com>
2024-08-14 18:22:56 +1000

    -> Added new MenuItem construct to handle MenuItem data.

---
+++ src/main/java/com/example/pixlpos/constructs/MenuItem.java
@@ -0,0 +1,40 @@
+package com.example.pixlpos.constructs;
+
+public class MenuItem {
+    private String itemName;
+    private String description;
+    private double price;
+
+    public MenuItem(String itemName, String description, double price) {
+        this.itemName = itemName;
+        this.description = description;
+        setPrice(price);
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            price = 0;
+        }
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/example/pixlpos/constructs/Users.java
@@ -0,0 +1,4 @@
+package com.example.pixlpos.constructs;
+
+public class Users {
+}
