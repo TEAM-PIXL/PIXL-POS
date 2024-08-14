9efb718622b3fc781c04651e31215024b91c2613
parkermmr <parkerrennie@outlook.com>
2024-08-14 17:42:21 +1000

    -> Added section for controllers. -> Initilized login controller and added login-screen.fxml

--- src/main/java/com/example/pixlpos/POSApplication.java
+++ src/main/java/com/example/pixlpos/POSApplication.java
@@ -7,12 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class POSApplication extends Application {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(POSApplication.class.getResource("/fxml/login-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Login Screen");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/pixlpos/controllers/LoginScreenController.java
@@ -0,0 +1,4 @@
+package com.example.pixlpos.controllers;
+
+public class LoginScreenController {
+}
