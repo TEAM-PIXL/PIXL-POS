feb3e8c8ebd3f2b44e7b05da0fe5bcadbfd15966
parkermmr <parkerrennie@outlook.com>
2024-08-18 20:35:07 +1000

    -> Added icon

--- src/main/java/com/example/pixlpos/POSApplication.java
+++ src/main/java/com/example/pixlpos/POSApplication.java
@@ -3,6 +3,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.io.InputStream;
 
 import java.io.IOException;
 
@@ -15,6 +17,12 @@ public class POSApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(POSApplication.class.getResource("/fxml/login-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle("Login Screen");
+        InputStream imageStream = getClass().getResourceAsStream("/icon.JPG");
+        if (imageStream == null) {
+            throw new IOException("Image resource not found: /icon.JPG");
+        }
+        Image image = new Image(imageStream);
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/pixlpos/POSApplicationLauncher.java
@@ -0,0 +1,7 @@
+package com.example.pixlpos;
+
+public class POSApplicationLauncher {
+    public static void main(String[] args) {
+        POSApplication.main(args);
+    }
+}
