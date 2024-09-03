1546234e3fdbb1048b1f2ed01412097b86d025a3
Heckus <Hecke.Joshua@gmail.com>
2024-09-03 21:26:24 +1000

    add<RDM-14>created connection for hardcoded menu items, list found on confluence

--- src/main/java/teampixl/com/pixlpos/controllers/waiterconsole/WaiterScreenController.java
+++ src/main/java/teampixl/com/pixlpos/controllers/waiterconsole/WaiterScreenController.java
@@ -20,37 +20,59 @@ public class WaiterScreenController extends GuiCommon {
 
     @FXML
     private TextField notes;
-
     @FXML
     private Button applynotes;
-
     @FXML
     private Button send;
-
     @FXML
     private Button restart;
-
     @FXML
     private Button voiditem;
-
     @FXML
     private Button itemcorrect;
-
     @FXML
     private Button logout;
-
     @FXML
     private Text timeordered;
-
     @FXML
     private Text tablenum;
-
     @FXML
     private Text timedue;
-
     @FXML
     private Text ordernum;
 
-
+    /* These buttons are hardcoded and connected to database items for the prototype*/
+    @FXML
+    private Button classic;
+    @FXML
+    private Button bbqbacon;
+    @FXML
+    private Button mushroomswiss;
+    @FXML
+    private Button spicy;
+    @FXML
+    private Button hawaiian;
+    @FXML
+    private Button veggie;
+    @FXML
+    private Button beyond;
+    @FXML
+    private Button mediterranean;
+    @FXML
+    private Button teriyaki;
+    @FXML
+    private Button breakfast;
+    @FXML
+    private Button coke;
+    @FXML
+    private Button fanta;
+    @FXML
+    private Button sprite;
+    @FXML
+    private Button icedtea;
+    @FXML
+    private Button icedcoffee;
+    @FXML
+    private Button ;
 
 }
