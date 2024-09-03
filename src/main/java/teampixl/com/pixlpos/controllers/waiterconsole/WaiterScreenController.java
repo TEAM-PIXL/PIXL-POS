e348e3552312a4adbbbb21906e38a259bb616c1a
Heckus <Hecke.Joshua@gmail.com>
2024-09-03 19:41:27 +1000

    feat<RDM-14>create fxml and controller files

---
+++ src/main/java/teampixl/com/pixlpos/controllers/waiterconsole/WaiterScreenController.java
@@ -0,0 +1,20 @@
+package teampixl.com.pixlpos.controllers.waiterconsole;
+
+import javafx.stage.Stage;
+import teampixl.com.pixlpos.common.GuiCommon;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import teampixl.com.pixlpos.constructs.Users;
+import teampixl.com.pixlpos.authentication.AuthenticationManager;
+import teampixl.com.pixlpos.database.DataStore;
+
+public class WaiterScreenController extends GuiCommon {
+    /*===================================================================================================================================================================================
+    Code Description:
+    This class is the controller for the login screen of the application. It handles the login functionality and navigation to different screens based on the user role.
+    At the moment, this is skeleton code and will be implemented further in the future.
+    ====================================================================================================================================================================================*/
+}
