c998bc0da4d10b78a3688868a25a778b61df94d8
Heckus <Hecke.Joshua@gmail.com>
2024-09-04 17:45:41 +1000

    add<RDM-45> controller

---
+++ src/main/java/teampixl/com/pixlpos/controllers/cookconsole/CookScreenController.java
@@ -0,0 +1,23 @@
+package teampixl.com.pixlpos.controllers.cookconsole;
+
+import teampixl.com.pixlpos.common.GuiCommon;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import teampixl.com.pixlpos.constructs.Users;
+import teampixl.com.pixlpos.authentication.AuthenticationManager;
+import teampixl.com.pixlpos.database.DataStore;
+
+public class CookScreenController extends GuiCommon {
+    /*===================================================================================================================================================================================
+    Code Description:
+    This class is the controller for the cook screen of the application. It handles displaying the orders and converting orders from database Infomation to visual dockets
+    ====================================================================================================================================================================================*/
+
+
+    @FXML
+    private Button ;
+}
