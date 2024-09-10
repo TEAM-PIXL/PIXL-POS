9ced4e0100f6862fc8ed15f973a52d1540a525e2
parkermmr <parkerrennie@outlook.com>
2024-09-10 21:57:19 +1000

    ci: test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,9 +18,7 @@ module PIXL.POS {
     requires javafx.fxml;
     requires javafx.graphics;
     requires jbcrypt;
-    requires org.junit.jupiter.api;
-    requires org.junit.platform.commons;
-    requires org.junit.platform.engine;
+    requires org.junit.jupiter;
 
     opens teampixl.com.pixlpos.application to javafx.fxml;
     opens teampixl.com.pixlpos.controllers.loginconsole to javafx.fxml;
