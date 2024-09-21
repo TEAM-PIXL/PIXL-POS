363c7d353b2f64034a18d4089e664eb04dd991d5
ZachCraw <zachariahcraw@gmail.com>
2024-09-21 13:57:03 +1000

    feat(): Initialising UserTest

---
+++ src/test/java/teampixl/com/pixlpos/database/UserTest.java
@@ -0,0 +1,16 @@
+package teampixl.com.pixlpos.database;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import teampixl.com.pixlpos.authentication.AuthenticationManager;
+import teampixl.com.pixlpos.models.Users;
+import teampixl.com.pixlpos.authentication.PasswordUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("ALL")
+class UserTest {
+
+    private DataStore dataStore;
+    private PasswordUtils passwordUtils;
+}
