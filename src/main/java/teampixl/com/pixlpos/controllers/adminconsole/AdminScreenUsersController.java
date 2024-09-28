7a4365e9f2504a3bde24cd9953ca467dcad4f4f8
Heckus <Hecke.Joshua@gmail.com>
2024-09-28 19:31:24 +1000

    add<>added base controllers, Home has basic controllers id'd moving onto id'ing the basics for all admin pahes

---
+++ src/main/java/teampixl/com/pixlpos/controllers/adminconsole/AdminScreenUsersController.java
@@ -0,0 +1,62 @@
+package teampixl.com.pixlpos.controllers.adminconsole;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import teampixl.com.pixlpos.common.GuiCommon;
+import teampixl.com.pixlpos.models.Users;
+import teampixl.com.pixlpos.models.MenuItem;
+import teampixl.com.pixlpos.database.DataStore;
+import teampixl.com.pixlpos.authentication.AuthenticationManager;
+import teampixl.com.pixlpos.database.api.UsersAPI;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import javafx.scene.layout.HBox;
+
+public class AdminScreenUsersController
+{
+    /*===================================================================================================================================================================================
+    Code Description:
+    This class is the controller for the home admin screen of the application.
+    ====================================================================================================================================================================================*/
+
+    /*
+    Shared Components
+     */
+
+    @FXML
+    private Text greeting;
+    @FXML
+    private TextField searchbar;
+    @FXML
+    private Label date;
+    @FXML
+    private Label time;
+
+    @FXML
+    private Button homebutton;
+    @FXML
+    private Button usersbutton;
+    @FXML
+    private Button menubutton;
+    @FXML
+    private Button stockbutton;
+    @FXML
+    private Button analyticsbutton;
+    @FXML
+    private Button logoutbutton;
+
+
+
+}
