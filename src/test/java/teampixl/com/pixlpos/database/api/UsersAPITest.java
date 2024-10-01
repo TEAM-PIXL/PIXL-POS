ff26c454cb769a2c69c61420875741d184cfc603
ZachCraw <zachariahcraw@gmail.com>
2024-10-01 19:00:30 +1000

    feat(RDM-24): Testing Putting User By Status

--- src/test/java/teampixl/com/pixlpos/database/api/UsersAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/UsersAPITest.java
@@ -160,4 +160,11 @@ class UsersAPITest {
         assertTrue(result.contains(StatusCode.SUCCESS));
     }
 
+    @Test
+    public void testPutUsersStatus() {
+        usersAPI.postUsers("John", "Doe", "johndoe", "Password1!", "johndoe@example.com", Users.UserRole.ADMIN, "Some info");
+        List<StatusCode> result = usersAPI.putUsersStatus("johndoe", false);
+        assertTrue(result.contains(StatusCode.SUCCESS));
+    }
+
 }
