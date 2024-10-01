6fcf60d27f20b36f2ca0d8a47cb202dd303bb6e5
ZachCraw <zachariahcraw@gmail.com>
2024-10-01 13:10:32 +1000

    feat(RDM-24): Adding in creation with null last name test

--- src/test/java/teampixl/com/pixlpos/models/UsersTest.java
+++ src/test/java/teampixl/com/pixlpos/models/UsersTest.java
@@ -46,6 +46,11 @@ class UsersTest {
     }
 
     @Test
+    void testUserCreationWithNullLastName() {
+        assertThrows(IllegalArgumentException.class, () -> new Users("John", null, "johndoe", "password", "", Users.UserRole.ADMIN));
+    }
+
+    @Test
     void testUserCreationWithNullUsername() {
         assertThrows(IllegalArgumentException.class, () -> new Users("John", "Doe", null, "password", "", Users.UserRole.ADMIN));
     }
