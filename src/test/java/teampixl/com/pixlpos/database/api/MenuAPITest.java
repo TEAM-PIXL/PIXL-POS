214ab10d2bae0d2ab740c363c8928f8435e79655
Jason Gunawan <jasongunawan2002@gmail.com>
2024-10-02 15:23:40 +1000

    feat(RDM-25): Testing Validate Menu Item Description

--- src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
@@ -17,10 +17,26 @@ public class MenuAPITest {
         assertEquals(StatusCode.INVALID_MENU_ITEM_NAME, menuAPI.validateMenuItemByName(""));
         assertEquals(StatusCode.INVALID_MENU_ITEM_NAME, menuAPI.validateMenuItemByName(" "));
         assertEquals(StatusCode.MENU_ITEM_NAME_TOO_SHORT, menuAPI.validateMenuItemByName("Fi"));
-        assertEquals(StatusCode.MENU_ITEM_NAME_TOO_LONG, menuAPI.validateMenuItemByName("abcdefghijklmnopqrstuvwxyznowyoulnowmyabcswontyoucomecandsingwithme"));
+        String LongName = "A".repeat(51);
+        assertEquals(StatusCode.MENU_ITEM_NAME_TOO_LONG, menuAPI.validateMenuItemByName(LongName));
         assertEquals(StatusCode.MENU_ITEM_NAME_CONTAINS_DIGITS, menuAPI.validateMenuItemByName("Soup12"));
         assertEquals(StatusCode.MENU_ITEM_NAME_CONTAINS_SPECIAL_CHARACTERS, menuAPI.validateMenuItemByName("Soup?"));
         assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByName("Soup"));
     }
+
+    @Test
+    void testValidateMenuItemByPrice(){
+        assertEquals(StatusCode.MENU_ITEM_PRICE_NEGATIVE, menuAPI.validateMenuItemByPrice(-1.00));
+        assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByPrice(1.00));
+    }
+
+    @Test
+    void testValidateMenuItemByDescription(){
+        String LongerName = "A".repeat(501);
+        assertEquals(StatusCode.MENU_ITEM_DESCRIPTION_TOO_LONG, menuAPI.validateMenuItemByDescription(LongerName));
+        assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByDescription("It's really tasty."));
+    }
+
+
 
 }
