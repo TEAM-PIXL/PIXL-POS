3831be1cb755c39899545dc76f94c23c30daeb74
Jason Gunawan <jasongunawan2002@gmail.com>
2024-10-02 15:49:21 +1000

    feat(RDM-25): Testing Validate Menu Item Notes

--- src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
@@ -1,15 +1,39 @@
 package teampixl.com.pixlpos.database.api;
+import teampixl.com.pixlpos.database.api.util.Exceptions;
 import teampixl.com.pixlpos.models.MenuItem;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import teampixl.com.pixlpos.database.api.util.StatusCode;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuAPITest {
 
     private static MenuAPI menuAPI;
+    private static String testMenuItemName;
+    private static MenuItem.ItemType testMenuItemType;
+    private static String testMenuItemDescription;
+    private static double testMenuItemPrice;
+    private static String testMenuItemNotes;
+    private static boolean testMenuItemActive;
+    private static MenuItem.DietaryRequirement testMenuItemDiet;
+
 
     @BeforeAll
     static void setUp() {
         menuAPI = MenuAPI.getInstance();
+        testMenuItemName = "Dog Food";
+        testMenuItemPrice = 23.00;
+        testMenuItemType = MenuItem.ItemType.MAIN;
+        testMenuItemDescription = "Cool Description";
+        testMenuItemNotes = "Notes";
+        testMenuItemActive = false;
+        testMenuItemDiet = MenuItem.DietaryRequirement.VEGAN;
+
+        List<StatusCode> postedStatusCodes = menuAPI.postMenuItem(testMenuItemName,testMenuItemPrice,testMenuItemActive,
+                testMenuItemType,testMenuItemDescription,testMenuItemNotes,testMenuItemDiet);
     }
 
     @Test
@@ -37,6 +61,17 @@ public class MenuAPITest {
         assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByDescription("It's really tasty."));
     }
 
+    @Test
+    void testValidateMenuItemByNotes(){
+        String LongerName = "A".repeat(501);
+        assertEquals(StatusCode.MENU_ITEM_DESCRIPTION_TOO_LONG, menuAPI.validateMenuItemByNotes(LongerName));
+        assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByNotes("Super Cool Notes."));
+    }
+
+    @Test
+    void testGetMenuItem(){
+        assertNotNull(menuAPI.getMenuItem(testMenuItemName));
+    }
 
 
 }
