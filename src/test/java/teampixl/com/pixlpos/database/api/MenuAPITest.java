cb4581025b47c2d620b1b35c189d28113779d0c8
Jason Gunawan <jasongunawan2002@gmail.com>
2024-10-02 15:19:41 +1000

    feat(RDM-25): Testing Validate Menu Item Name

--- src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
@@ -1,7 +1,8 @@
 package teampixl.com.pixlpos.database.api;
+import teampixl.com.pixlpos.models.MenuItem;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+import teampixl.com.pixlpos.database.api.util.StatusCode;
 public class MenuAPITest {
 
     private static MenuAPI menuAPI;
@@ -9,6 +10,17 @@ public class MenuAPITest {
     @BeforeAll
     static void setUp() {
         menuAPI = MenuAPI.getInstance();
+    }
+
+    @Test
+    void testValidateMenuItemByName(){
+        assertEquals(StatusCode.INVALID_MENU_ITEM_NAME, menuAPI.validateMenuItemByName(""));
+        assertEquals(StatusCode.INVALID_MENU_ITEM_NAME, menuAPI.validateMenuItemByName(" "));
+        assertEquals(StatusCode.MENU_ITEM_NAME_TOO_SHORT, menuAPI.validateMenuItemByName("Fi"));
+        assertEquals(StatusCode.MENU_ITEM_NAME_TOO_LONG, menuAPI.validateMenuItemByName("abcdefghijklmnopqrstuvwxyznowyoulnowmyabcswontyoucomecandsingwithme"));
+        assertEquals(StatusCode.MENU_ITEM_NAME_CONTAINS_DIGITS, menuAPI.validateMenuItemByName("Soup12"));
+        assertEquals(StatusCode.MENU_ITEM_NAME_CONTAINS_SPECIAL_CHARACTERS, menuAPI.validateMenuItemByName("Soup?"));
+        assertEquals(StatusCode.SUCCESS, menuAPI.validateMenuItemByName("Soup"));
     }
 
 }
