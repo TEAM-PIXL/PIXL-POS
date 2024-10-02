0ca7a6fbb0cd245c2e2b11d4fded99bf0086bc50
Jason Gunawan <jasongunawan2002@gmail.com>
2024-10-02 15:50:53 +1000

    feat(RDM-25): Testing Put Menu Item Type

--- src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/MenuAPITest.java
@@ -85,5 +85,9 @@ public class MenuAPITest {
                 menuAPI.putMenuItemPrice(testMenuItemName, 12.00)));
     }
 
-
+    @Test
+    void testPutMenuItemType(){
+        assertTrue(Exceptions.isSuccessful(
+                menuAPI.putMenuItemItemType(testMenuItemName, MenuItem.ItemType.DESSERT)));
+    }
 }
