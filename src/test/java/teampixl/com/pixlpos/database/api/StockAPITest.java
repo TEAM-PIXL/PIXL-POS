18060be1b0e83dec6065dbf0dfbf5278c19fcd1d
ZachCraw <zachariahcraw@gmail.com>
2024-10-03 13:06:20 +1000

    test(RDM-24): Creating tests for Validations

--- src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import teampixl.com.pixlpos.database.api.StockAPI;
 import teampixl.com.pixlpos.database.api.util.Exceptions;
 import teampixl.com.pixlpos.database.api.util.StatusCode;
+import teampixl.com.pixlpos.models.Ingredients;
 import teampixl.com.pixlpos.models.Stock;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StockAPITest {
 
@@ -20,14 +22,15 @@ class StockAPITest {
     private double testStockQuantity;
     private boolean testOnOrderStatus;
     private int testCounter;
+    private String[] testIngredientIDs;
 
     @BeforeEach
     void setUp() {
         stockAPI = StockAPI.getInstance();
         testCounter++;
 
-        testStockID = "testID" + testCounter;
-        testStockStatus = Stock.StockStatus.INSTOCK
+        testStockID = "testIngredientID" + testCounter;
+        testStockStatus = Stock.StockStatus.INSTOCK;
         testUnitType = Stock.UnitType.KG;
         testStockQuantity = 100.0;
         testOnOrderStatus = false;
@@ -36,7 +39,28 @@ class StockAPITest {
         assertTrue(Exceptions.isSuccessful(StatusCodes));
     }
 
+    @Test
+    void testValidStockByIgredientID() {
+        StatusCode statusCode = stockAPI.validateStockByIngredientID(testStockID);
+        assertEquals(StatusCode.SUCCESS, statusCode);
+    }
 
+    @Test
+    void testValidStockByQuantity() {
+        StatusCode statusCode = stockAPI.validateStockByQuantity(testStockQuantity);
+        assertEquals(StatusCode.SUCCESS, statusCode);
+    }
 
+    @Test
+    void testValidStockByUnitType() {
+        StatusCode statusCode = stockAPI.validateStockByUnitType(testUnitType);
+        assertEquals(StatusCode.SUCCESS, statusCode);
+    }
+
+    @Test
+    void testValidateStockByStockStatus() {
+        StatusCode statusCode = stockAPI.validateStockByStockStatus(testStockStatus);
+        assertEquals(StatusCode.SUCCESS, statusCode);
+    }
 
 }
