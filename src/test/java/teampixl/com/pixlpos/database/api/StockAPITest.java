80e793c6ff2a3d002102a9928932e51c14fd23ba
ZachCraw <zachariahcraw@gmail.com>
2024-10-03 12:49:24 +1000

    test(RDM-24): Creating stock entry BeforeEach()

--- src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
@@ -3,7 +3,13 @@ package teampixl.com.pixlpos.database.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import teampixl.com.pixlpos.database.api.StockAPI;
+import teampixl.com.pixlpos.database.api.util.Exceptions;
+import teampixl.com.pixlpos.database.api.util.StatusCode;
 import teampixl.com.pixlpos.models.Stock;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StockAPITest {
 
@@ -13,11 +19,24 @@ class StockAPITest {
     private Stock.UnitType testUnitType;
     private double testStockQuantity;
     private boolean testOnOrderStatus;
+    private int testCounter;
 
     @BeforeEach
     void setUp() {
         stockAPI = StockAPI.getInstance();
+        testCounter++;
 
+        testStockID = "testID" + testCounter;
+        testStockStatus = Stock.StockStatus.INSTOCK
+        testUnitType = Stock.UnitType.KG;
+        testStockQuantity = 100.0;
+        testOnOrderStatus = false;
+
+        List<StatusCode> StatusCodes = stockAPI.postStock(testStockID, testStockStatus, testUnitType, testStockQuantity, testOnOrderStatus);
+        assertTrue(Exceptions.isSuccessful(StatusCodes));
     }
+
+
+
 
 }
