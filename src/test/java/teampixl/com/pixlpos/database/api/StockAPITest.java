b491279aba36095bc08f73e0b5adc76c5419c617
ZachCraw <zachariahcraw@gmail.com>
2024-10-04 11:40:05 +1000

    test(RDM-24): Creating a post method for ingredientsAPI

--- src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
+++ src/test/java/teampixl/com/pixlpos/database/api/StockAPITest.java
@@ -1,47 +1,63 @@
 package teampixl.com.pixlpos.database.api;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import teampixl.com.pixlpos.database.api.StockAPI;
 import teampixl.com.pixlpos.database.api.util.Exceptions;
 import teampixl.com.pixlpos.database.api.util.StatusCode;
-import teampixl.com.pixlpos.models.Ingredients;
+import teampixl.com.pixlpos.database.api.IngredientsAPI;
 import teampixl.com.pixlpos.models.Stock;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class StockAPITest {
 
     private static StockAPI stockAPI;
-    private String testStockID;
+    private String testIngredientID;
+    private String testIngredientName;
     private Stock.StockStatus testStockStatus;
     private Stock.UnitType testUnitType;
     private double testStockQuantity;
     private boolean testOnOrderStatus;
     private int testCounter;
-    private String[] testIngredientIDs;
+    private IngredientsAPI ingredientsAPI;
+
+    private String postIngredient() {
+        String IngredientID;
+        List<StatusCode> StatusCodes = ingredientsAPI.postIngredient(testIngredientName);
+        assertTrue(Exceptions.isSuccessful(StatusCodes));
+
+        IngredientID = ingredientsAPI.keySearch(testIngredientName);
+        assertNotEquals(null, IngredientID);
+
+        return IngredientID;
+    }
 
     @BeforeEach
     void setUp() {
         stockAPI = StockAPI.getInstance();
         testCounter++;
 
-        testStockID = "testIngredientID" + testCounter;
+        testIngredientName = "testIngredientID" + testCounter;
         testStockStatus = Stock.StockStatus.INSTOCK;
         testUnitType = Stock.UnitType.KG;
         testStockQuantity = 100.0;
         testOnOrderStatus = false;
 
-        List<StatusCode> StatusCodes = stockAPI.postStock(testStockID, testStockStatus, testUnitType, testStockQuantity, testOnOrderStatus);
+        testIngredientID = postIngredient();
+
+        List<StatusCode> StatusCodes = stockAPI.postStock(testIngredientID, testStockStatus, testUnitType, testStockQuantity, testOnOrderStatus);
         assertTrue(Exceptions.isSuccessful(StatusCodes));
     }
 
     @Test
-    void testValidStockByIgredientID() {
-        StatusCode statusCode = stockAPI.validateStockByIngredientID(testStockID);
+    void testValidStockByIngredientID() {
+        StatusCode statusCode = stockAPI.validateStockByIngredientID(testIngredientID);
         assertEquals(StatusCode.SUCCESS, statusCode);
     }
 
