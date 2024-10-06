4b761f2f45857f9a4bc2ec597b93a33c42f4d478
parkermmr <parkerrennie@outlook.com>
2024-10-06 14:26:57 +1000

    docs(RDM-21): Updated the javadoc documentation for DataManager and its interface.

--- src/main/java/teampixl/com/pixlpos/models/tools/IDataManager.java
+++ src/main/java/teampixl/com/pixlpos/models/tools/IDataManager.java
@@ -6,22 +6,26 @@ import java.util.Map;
  * Interface for data managers.
  */
 public interface IDataManager {
+
     /**
      * Get the data map.
      * @return Map of data.
      */
     Map<String, Object> getData();
+
     /**
      * Get the metadata wrapper.
      * @return MetadataWrapper object.
      */
     MetadataWrapper getMetadata();
+
     /**
      * Update the value of a metadata key.
      * @param key Key to update value of.
      * @param value Value to update.
      */
     void updateMetadata(String key, Object value);
+
     /**
      * Set the value of a data key.
      * @param key Key to set value of.
