c67cadc39c88a420fce493b1e3f65728ee459cb5
ZachCraw <zachariahcraw@gmail.com>
2024-10-12 16:55:27 +1000

    feat(): Working stock lowest status widget

--- src/main/java/teampixl/com/pixlpos/controllers/adminconsole/AdminScreenHomeController.java
+++ src/main/java/teampixl/com/pixlpos/controllers/adminconsole/AdminScreenHomeController.java
@@ -13,27 +13,26 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.animation.AnimationTimer;
 import teampixl.com.pixlpos.common.GuiCommon;
+import teampixl.com.pixlpos.database.api.*;
 import teampixl.com.pixlpos.models.Users;
 import teampixl.com.pixlpos.models.MenuItem;
 import teampixl.com.pixlpos.database.DataStore;
 import teampixl.com.pixlpos.authentication.AuthenticationManager;
-import teampixl.com.pixlpos.database.api.UsersAPI;
-import teampixl.com.pixlpos.database.api.MenuAPI;
-import teampixl.com.pixlpos.database.api.OrderAPI;
-import teampixl.com.pixlpos.database.api.UserStack;
 import teampixl.com.pixlpos.database.api.util.Exceptions;
 import teampixl.com.pixlpos.database.api.util.StatusCode;
 
 import teampixl.com.pixlpos.models.Stock;
-import teampixl.com.pixlpos.database.api.StockAPI;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 
 import javafx.scene.layout.HBox;
@@ -46,6 +45,9 @@ public class AdminScreenHomeController {
     private final UserStack userStack = UserStack.getInstance();
     Users currentuser = userStack.getCurrentUser();
     String firstName = currentuser.getMetadata().metadata().get("first_name").toString();
+    private final StockAPI stockAPI = StockAPI.getInstance();
+    private final IngredientsAPI ingredientsAPI = IngredientsAPI.getInstance();
+    private final DataStore dataStore = DataStore.getInstance();
     /*
     Shared Components
      */
@@ -133,9 +135,7 @@ public class AdminScreenHomeController {
         addNoteToListView(noteslistview, "2", "Jane Doe", "This is a note");
         addNoteToListView(noteslistview, "3", "Michael Doe", "This is a note");
         stockalertlistview.getItems().clear();
-        addStockAlertListView(stockalertlistview, "1", "Cheese",Stock.StockStatus.INSTOCK, false);
-        addStockAlertListView(stockalertlistview, "2", "Bread",Stock.StockStatus.LOWSTOCK, true);
-        addStockAlertListView(stockalertlistview, "3", "Tomato",Stock.StockStatus.NOSTOCK, false);
+        calculateStockWidget();
         userloglistview.getItems().clear();
         adduserlogToListView(userloglistview, "1", "13/12/2020", "John Smith", "10:02am", "0hrs 6mins");
         adduserlogToListView(userloglistview, "2", "12/12/2020", "Jane Doe", "9:02am", "5hrs 6mins");
@@ -203,6 +203,20 @@ public class AdminScreenHomeController {
     @FXML
     protected void onLogoutButtonClick() {
         GuiCommon.logout(logoutbutton);
+    }
+
+    private void calculateStockWidget() {
+        List<Stock> stockItems = dataStore.readStock();
+        stockItems.sort(Comparator.comparing(stock -> (Double) stock.getData().get("numeral")));
+        List<Stock> leastInStockItems = stockItems.stream().limit(3).collect(Collectors.toList());
+
+        stockalertlistview.getItems().clear();
+        for (Stock stock : leastInStockItems) {
+            addStockAlertListView(stockalertlistview, stock.getMetadataValue("stock_id").toString(),
+                    ingredientsAPI.reverseKeySearch(stock.getMetadataValue("ingredient_id").toString()),
+                    (Stock.StockStatus) stock.getMetadataValue("stockStatus"),
+                    (Boolean) stock.getMetadataValue("onOrder"));
+        }
     }
 
 
