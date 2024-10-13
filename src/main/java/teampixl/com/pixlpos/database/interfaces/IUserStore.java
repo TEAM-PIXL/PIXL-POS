e5063a1101f4c1d23c0e9fce0538b10bf10af34e
parkermmr <parkerrennie@outlook.com>
2024-10-13 13:19:41 +1000

    docs(): Updated jar docs for interfaces on stock and users.

--- src/main/java/teampixl/com/pixlpos/database/interfaces/IUserStore.java
+++ src/main/java/teampixl/com/pixlpos/database/interfaces/IUserStore.java
@@ -3,10 +3,33 @@ package teampixl.com.pixlpos.database.interfaces;
 import teampixl.com.pixlpos.models.Users;
 import javafx.collections.ObservableList;
 
+/**
+ * The IUserStore interface is used to define the methods that will be used to interact with the Users table in the database.
+ */
 public interface IUserStore {
+
+    /**
+     * The readUsers method is used to read all the users from the Users table in the database.
+     * @return ObservableList<Users> - The list of users in the Users table.
+     */
     ObservableList<Users> readUsers();
+
+    /**
+     * The createUser method is used to create a new user in the Users table in the database.
+     * @param user - The user to be created.
+     */
     void createUser(Users user);
+
+    /**
+     * The deleteUser method is used to delete a user from the Users table in the database.
+     * @param user - The user to be deleted.
+     */
     void deleteUser(Users user);
+
+    /**
+     * The updateUser method is used to update a user in the Users table in the database.
+     * @param user - The user to be updated.
+     */
     void updateUser(Users user);
 }
 
